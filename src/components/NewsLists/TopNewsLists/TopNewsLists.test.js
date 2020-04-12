9a5871e571fef7fae51274a7fcf1039a8d33acc5
Rakesh Kumar <rakeshpersonal@gmail.com>
2020-04-12 14:36:53 +0530

    mock axios

--- src/components/NewsLists/TopNewsLists/TopNewsLists.test.js
+++ src/components/NewsLists/TopNewsLists/TopNewsLists.test.js
@@ -2,6 +2,9 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import TopNewsLists from "./TopNewsLists";
 import ListItems from "./../ListItems";
+import axios from "axios";
+// jest.mock("axios");
+jest.mock("../../../utils/requestService");
 
 describe("Top News Lists Component", () => {
   it("should renders component without crashing", () => {
