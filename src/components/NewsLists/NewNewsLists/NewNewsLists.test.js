0ac862940ada85269f2335b81b4f15202c12e656
Rakesh Kumar <rakeshpersonal@gmail.com>
2020-04-12 12:24:33 +0530

    added some more test cases

--- src/components/NewsLists/NewNewsLists/NewNewsLists.test.js
+++ src/components/NewsLists/NewNewsLists/NewNewsLists.test.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
-import TopNewsLists from "./TopNewsLists";
+import NewNewsLists from "./NewNewsLists";
 import ListItems from "./../ListItems";
 
-describe("Top News Lists Component", () => {
+describe("New News Lists Component", () => {
   it("should renders component without crashing", () => {
-    const wrapper = shallow(<TopNewsLists />);
+    const wrapper = shallow(<NewNewsLists />);
     expect(wrapper.exists()).toBe(true);
   });
 
   it("should renders list items component", () => {
-    const wrapper = mount(<TopNewsLists />);
+    const wrapper = mount(<NewNewsLists />);
     expect(wrapper.find(ListItems).length).toEqual(1);
   });
 });
